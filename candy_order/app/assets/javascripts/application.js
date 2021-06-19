e9e6a7d7ea22100aafbae638452020bdc84c2d67
takapamu <takapamukyary0129@gmail.com>
2021-06-19 15:20:20 +0000

    ローディング機能

--- candy_order/app/assets/javascripts/application.js
+++ candy_order/app/assets/javascripts/application.js
@@ -43,3 +43,13 @@ $(function() {
   });
 });
 
+// 読み込んだらフェードアウト
+$(window).load(function () {
+    // 消えるタイミングはお好みで
+    $('.loading').delay(1500).fadeOut(300);
+});
+// 10秒待っても読み込みが終わらない時は強制的にローディング画面をフェードアウト
+function stopload(){
+    $('.loading').delay(1000).fadeOut(700);
+}
+setTimeout('stopload()',10000);
