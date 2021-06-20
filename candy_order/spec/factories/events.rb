d1afb6c47acb508d71b3dc0f8573621b258f2fe0
takapamu <takapamukyary0129@gmail.com>
2021-06-20 12:04:26 +0000

    Event model Rspec

---
+++ candy_order/spec/factories/events.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+ factory :event do
+   title             {"test"}
+   content           {"test"}
+   start_time        {"2021.3"}
+ end
+end
--- candy_order/spec/models/event_spec.rb
+++ candy_order/spec/models/event_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Event do
+  describe '#create' do
+    it 'イベント登録' do
+      event = build(:event)
+      event.valid?
+      expect(event).to be_valid
+    end
+    
+    it "タイトルバリデーション" do
+    event = build(:event,title: nil)
+    event.valid?
+    expect(event.errors[:title]).to include("を入力してください") #日本語で書く
+   end
+   
+   it "内容バリデーション" do
+    event = build(:event,content: nil)
+    event.valid?
+    expect(event.errors[:content]).to include("を入力してください") #日本語で書く
+   end
+   
+    it "日にちバリデーション" do
+    event = build(:event,start_time: nil)
+    event.valid?
+    expect(event.errors[:start_time]).to include("を入力してください") #日本語で書く
+   end
+  end
 end
