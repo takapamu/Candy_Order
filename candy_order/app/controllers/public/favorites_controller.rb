752dd7363698c72e54e1bc1c09d972dc62e4e706
takapamu <takapamukyary0129@gmail.com>
2021-06-21 04:57:23 +0000

    お気にいり　　非同期

--- candy_order/app/controllers/public/favorites_controller.rb
+++ candy_order/app/controllers/public/favorites_controller.rb
@@ -1,15 +1,14 @@
 class  Public::FavoritesController < ApplicationController
 
     def create
+      @product = Product.find(params[:product_id])
       @favorite = current_shop.favorites.create(product_id: params[:product_id])
-      redirect_back(fallback_location: root_path)
     end
     
     def destroy
       @product = Product.find(params[:product_id])
       @favorite = current_shop.favorites.find_by( product_id:  @product.id)
       @favorite.destroy
-      redirect_back(fallback_location: root_path)
     end
     
 end
